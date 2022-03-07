4af6a67d3bf2933718dce1a72b060cfd6b96c047
0000apa0000 <99506016+0000apa0000@users.noreply.github.com>
2022-03-07 23:22:17 +0330

    16/12/1400
    alirea

---
+++ 2Dtank/Assets/tankcontroler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tankcontroler : MonoBehaviour
+{
+
+    Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rigid.velocity = direction * 3;
+
+    }
+
+ 
+}
