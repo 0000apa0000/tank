579c7030f967c4ba2327e459bbc7d19c0fdf8055
0000apa0000 <99506016+0000apa0000@users.noreply.github.com>
2022-03-14 22:50:57 +0330

    1400-12-23-headrot

---
+++ 2Dtank/Assets/Script/TankScript/headtank.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class headtank : MonoBehaviour
+{
+    public GameObject Headtank;
+    public Joystick joystick;
+    Vector2 rot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rot = new Vector2(joystick.Horizontal, joystick.Vertical);
+
+        Headtank.transform.rotation = Quaternion.LookRotation(transform.forward, rot);
+    }
+}
